ddb08aeead65c382a28e39731f30962a772b0011
Ranczo <ramazan.acikgz@gmail.com>
2024-09-28 13:30:42 +0200

    4Day

---
+++ 100DaysPython/4Day/lists.py
@@ -0,0 +1,17 @@
+fruits = ["Cherry", "Apple", "Pear"]
+#It is in order 0, 1, 2 indexes
+
+print(fruits[1])
+#Apple
+print(fruits[-1])
+#Pear
+
+states_of_america = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+                     "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+                     "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
+                     "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", 
+                     "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+                     "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "Wesh Virginia", "Wisconson", "Wyoming"]
+
+states_of_america[-1] = "WYOMING" #Change the item
+states_of_america.append("RAland") #Adding new item
---
+++ 100DaysPython/4Day/randomisation.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+# local module
+
+#random_integer = random.randint(1, 10)
+
+#print(random_integer)
+
+#print(my_module.my_favorite_number)
+
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
