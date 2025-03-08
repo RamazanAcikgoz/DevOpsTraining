d7c25ed25b83480e988c3368040b2689b955fc4c
Ranczo <ramazan.acikgz@gmail.com>
2025-03-08 11:45:59 +0100

    12Day

--- 100DaysPython/12Day/globalconstants.py
+++ 100DaysPython/12Day/globalconstants.py
@@ -3,6 +3,7 @@
 # You can define global constants in your code file for easy access
 # Their job is meant to be 'set and forget' 
 # Global constants are normally declared in ALL_CAPITAL
+# RA
 PI = 3.14159
 GOOGLE_URL = "https://wwww.google.com"
 
