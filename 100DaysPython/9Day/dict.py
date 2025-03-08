0d4b077ea51eb1825e9bf08d9094d874b0b32802
Ranczo <ramazan.acikgz@gmail.com>
2025-03-08 11:43:46 +0100

    9Day

--- 100DaysPython/9Day/dict.py
+++ 100DaysPython/9Day/dict.py
@@ -28,4 +28,5 @@ characters["a"] = 6
 # Loop through dict
 for thing in characters:
     print(thing) # It will print out just keys 
-    print(characters[thing]) # it will give value as well 
+    print(characters[thing]) # it will give value as well
+#RA
