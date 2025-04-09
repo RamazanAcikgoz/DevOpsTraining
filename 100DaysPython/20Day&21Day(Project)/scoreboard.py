8c86cf13cebfdf7f1ea0d459bdbfcef279ce97bb
Ranczo <ramazan.acikgz@gmail.com>
2025-04-09 14:19:51 +0200

    Update highscore

--- 100DaysPython/20Day&21Day(Project)/scoreboard.py
+++ 100DaysPython/20Day&21Day(Project)/scoreboard.py
@@ -8,6 +8,9 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.highscore = 0
+        # Angela's version
+        # with open("data.txt") as data:
+        #   self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -16,11 +19,18 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+        with open("high_score.txt", mode= "r") as highscore_file:
+            new_highscore = highscore_file.read()
+        self.write(f"Score: {self.score} High Score: {new_highscore}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("high_score.txt", mode= "w") as highscore_file:
+                highscore_file.write(f"{self.highscore}")
+            # Angela's version
+            # with open("data.txt", mode= "w") as data:
+            #   data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
